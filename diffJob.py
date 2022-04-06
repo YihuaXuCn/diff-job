642969cb6162575cce06294445683285724c49f9
YihuaXuCn <yxu131@hawk.iit.edu>
2022-04-06 14:14:37 -0500

    update for linear method

--- diffJob.py
+++ diffJob.py
@@ -2,9 +2,15 @@ import argparse, os, json
 from subprocess import run, TimeoutExpired
 from concurrent.futures import ProcessPoolExecutor
 
+paralMode = False
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', "--directory", type=str, help="path of a folder", required=True)
-parser.add_argument('-o', "--output", type=str, help="output overtime file name", required=True)
+parser.add_argument('-ot', "--overtime", type=str, help="output overtime file name", required=True)
+parser.add_argument('-ds', "--diffScore", type=str, help="output diffScores", required=True)
+if not paralMode:
+    parser.add_argument('-i', "--indexOfData", type=int, help="index of data slices", required=True)
+    parser.add_argument('-s', "--sliceNumb", type=int, help="total numbers of data slices", required=True)
 args = parser.parse_args()
 
 def runCmdFind(folder, fileType):
@@ -55,9 +61,22 @@ def getFileLns(file):
 
 
 evaluatedFns = runCmdFind('34DappsFns', 'fn')
-folder = args.directory
-datasetFns = runCmdFind(folder, 'fn')
 
+folder = args.directory
+# splite dataset into pices for speeding up in linear mode
+if not paralMode:
+    datasetFns = runCmdFind(folder, 'fn')
+    i = args.index
+    s = args.sliceNumb
+    fixedNumb = len(datasetFns)//(s-1)
+    if (i+1)*fixedNumb > len(datasetFns) :
+        datasetFns = datasetFns[i*fixedNumb :]
+    else:
+        datasetFns = datasetFns[i*fixedNumb : (i+1)*fixedNumb]
+else:
+    datasetFns = runCmdFind(folder, 'fn')
+
+# convinient for parallel mode 
 filePairList = []
 if os.path.exists('nameErr.txt'):
     run(['rm', 'nameErr.txt'])
@@ -65,19 +84,31 @@ for file1 in evaluatedFns:
     for file2 in datasetFns:
         filePairList.append((file1,file2))
 
-with ProcessPoolExecutor(max_workers=8) as executor:
-    failed = []
-    pool = executor.map(diffOneFile, filePairList)
-
-    for res in pool:
-        if res == None:
+failed = []
+if not paralMode:
+    for difFilePair in filePairList:
+        timeoutFile = diffOneFile(difFilePair)
+        if not timeoutFile:
             continue
-        failed.append('{}\t{}\n'.format('timeover',res))
-    # failed = [f for f in failed]
+        else:
+            failed.append('{}\t{}\n'.format('timeover',timeoutFile))
+    if len(failed) != 0:
+        with open(args.overtime,'w+') as fd:
+            fd.writelines(failed)
+else:
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        failed = []
+        pool = executor.map(diffOneFile, filePairList)
 
-    fd = open(args.output,'w+')
-    fd.writelines(failed)
-    fd.close()
+        for res in pool:
+            if res == None:
+                continue
+            failed.append('{}\t{}\n'.format('timeover',res))
+        # failed = [f for f in failed]
+        if len(failed) != 0:
+            fd = open(args.overtime,'w+')
+            fd.writelines(failed)
+            fd.close()
 
 # calculate diff scores
 evalFnLnCntDict = {}
@@ -93,14 +124,26 @@ print("diff files :{}".format(len(allDifFiles)))
 if os.path.exists('diffScores.json'):
     run(['rm', '-f', 'diffScores.json'])
 
-with ProcessPoolExecutor(max_workers=8) as executor:
-    pool = executor.map(getFileLns, allDifFiles)
-    for result in pool:
-        difFileName, lnCnt = result
+difScrFile = args.diffScore
+if not paralMode:
+    for difFile in allDifFiles:
+        _, lnCnt = getFileLns(allDifFiles)
         evalFn = os.path.basename(difFileName)[:-len('.dif')].split('_vs_')[0]
         evalFnLnCnt = evalFnLnCntDict[evalFn]
         diffScoresDict[difFileName] = int(lnCnt)/int(evalFnLnCnt)
     sortedDiffScores = sorted(diffScoresDict.items(), key=lambda x: x[1])
     sortedDiffScoresDict = { e[0]: e[1] for e in sortedDiffScores}
-    with open('diffScores.json', 'w+') as outfile:
+    with open(difScrFile, 'w+') as outfile:
         json.dump(sortedDiffScoresDict, outfile, indent=4)
+else:
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        pool = executor.map(getFileLns, allDifFiles)
+        for result in pool:
+            difFileName, lnCnt = result
+            evalFn = os.path.basename(difFileName)[:-len('.dif')].split('_vs_')[0]
+            evalFnLnCnt = evalFnLnCntDict[evalFn]
+            diffScoresDict[difFileName] = int(lnCnt)/int(evalFnLnCnt)
+        sortedDiffScores = sorted(diffScoresDict.items(), key=lambda x: x[1])
+        sortedDiffScoresDict = { e[0]: e[1] for e in sortedDiffScores}
+        with open('diffScores.json', 'w+') as outfile:
+            json.dump(sortedDiffScoresDict, outfile, indent=4)
--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,9 @@ from concurrent.futures import ProcessPoolExecutor
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', "--directory", type=str, help="path of a folder", required=True)
-parser.add_argument('-o', "--output", type=str, help="output file name", required=True)
+parser.add_argument('-i', "--indexOfData", type=int, help="index of data slices", required=True)
+parser.add_argument('-s', "--sliceNumb", type=int, help="total numbers of data slices", required=True)
+# parser.add_argument('-o', "--output", type=str, help="output file name", required=True)
 # parser.add_argument('-l', "--logDir", type=str, help="log folder name", required=True)
 args = parser.parse_args()
 
@@ -15,19 +17,20 @@ def preprocessOneFile(file):
     cmdStr = "sed -i s/[[:blank:]]//g;/^$/d"
     cmd = cmdStr.split(' ') + [file]
     try:
-        process = run(cmd, capture_output=True, timeout = 60)
+        process = run(cmd, timeout = 60)
     except TimeoutExpired:
-        state=1
-        return state, file
-    else:
-        # print("sed cmd returncode :{}".format(process.returncode))
-        if process.returncode != 0:
-            state=2
-            return process.stderr.decode('utf-8'), file
-        else:
-            # logfd.close()
-            state=3
-            return state, None
+        raise
+    #     state=1
+    #     return state, file
+    # else:
+    #     # print("sed cmd returncode :{}".format(process.returncode))
+    #     if process.returncode != 0:
+    #         state=2
+    #         return process.stderr.decode('utf-8'), file
+    #     else:
+    #         # logfd.close()
+    #         state=3
+    #         return state, None
 
 
 folder = args.directory
@@ -37,29 +40,41 @@ process = run(cmd, capture_output=True)
 # print("find cmd returncode :{}".format(process.returncode))
 files = process.stdout.decode('utf-8').split('\n')[:-1]
 
-with ProcessPoolExecutor(max_workers=8) as executor:
-    failed = []
-    # folder = args.directory
-    # files = os.listdir(folder)
-    # logdir = os.path.join(folder, args.logDir)
-    # if not os.path.exists(logdir):
-    #       run(['mkdir', '-p', logdir])
-    # files = [os.path.join(folder,f) for f in files]
-    # end = round(len(files)/10)
-    # files = files[:end]
-    # print(files[0])
-    # preprocessOneFile(files[0])
-    pool = executor.map(preprocessOneFile, files)
-    for res in pool:
-        if res[1] == None:
-            continue
-        failed.append('{}\t{}\n'.format(res[0],res[1]))
-    # failed = [f for f in failed]
+i = args.indexOfData
+s = args.sliceNumb
+sliceLen = len(files)//(s-1)
+print("total files : {} \t sliceLen : {} \t index : {}".format(len(files), sliceLen, i))
+dataSlice = files[sliceLen*i : sliceLen*(i+1)] if i < (s-1) else files[sliceLen*i:]
+for i, fnFile in enumerate(dataSlice, start=1):
+    preprocessOneFile(fnFile)
+    if i % 20000 == 0:
+        print("{} files have been done".format(i))
 
-    fd = open(args.output,'w+')
-    # fd.write(str(end))
-    # for f in failed:
-    #     fd.writelines(failed)
-    fd.writelines(failed)
-    fd.close()
+
+
+# with ProcessPoolExecutor(max_workers=4) as executor:
+#     # folder = args.directory
+#     # files = os.listdir(folder)
+#     # logdir = os.path.join(folder, args.logDir)
+#     # if not os.path.exists(logdir):
+#     #       run(['mkdir', '-p', logdir])
+#     # files = [os.path.join(folder,f) for f in files]
+#     # end = round(len(files)/10)
+#     # files = files[:end]
+#     # print(files[0])
+#     # preprocessOneFile(files[0])
+#     pool = executor.map(preprocessOneFile, files)
+    # failed = []
+    # for res in pool:
+    #     if res[1] == None:
+    #         continue
+    #     failed.append('{}\t{}\n'.format(res[0],res[1]))
+    # # failed = [f for f in failed]
+
+    # fd = open(args.output,'w+')
+    # # fd.write(str(end))
+    # # for f in failed:
+    # #     fd.writelines(failed)
+    # fd.writelines(failed)
+    # fd.close()
 
